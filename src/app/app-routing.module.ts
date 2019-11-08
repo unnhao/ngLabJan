a9f048842b8d9c81697f414094b5379522ebc9cd
unnhao <unnhao@gmail.com>
2019-11-08 16:44:52 +0800

    step-0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ValidationComponent } from './validation/validation.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ValidationComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
